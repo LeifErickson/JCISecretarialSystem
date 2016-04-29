de44ca93ac787b2e34e9e671bd5d2b35db9145d0
Leif Erickson de Gracia <Leif Erickson de Gracia>
2016-04-29 13:45:31 +0800

    added changes from albert

---
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Project as Project;
+use Illuminate\Http\Request;
+use DB;
+use DateTime;
+
+class AttendanceController extends Controller
+{
+	public function index($id)
+	{			
+		$result = DB::select('SELECT `events_attended`.`member_id`,`firstname`, `lastname`, `middlename`,`status`,`events_attended`.`year`
+					FROM `events_attended`,`members`  WHERE 
+					`events_attended`.`project_id` = ? AND 
+					`members`.`id` = `events_attended`.`member_id`',[$id]);
+		
+		$info = DB::select('SELECT * FROM `project`  WHERE  `id`=?',[$id]);
+		
+		$members = DB::select('SELECT `members`.`id`,`firstname`,`lastname`,`middlename` 
+										FROM `members` 
+										WHERE 
+											NOT EXISTS (SELECT `events_attended`.`id` FROM `events_attended`
+																  WHERE `events_attended`.`project_id` = ? AND
+											 `members`.`id` = `events_attended`.`member_id` ); ',[$id]);
+		
+		return view('admin.attendance.index', ['data' => $result, 'info' => $info, 'data_id'=> $id,'mems'=> $members]);
+	}
+	
+	 public function store($project_id,$member_id)
+    {
+		$now = new DateTime();
+		DB::insert('INSERT INTO `events_attended`(`member_id`, `project_id`, `year`) 
+		VALUES (?,?,?)', [$member_id, $project_id,$now]);
+		return redirect('admin/attendance/'.$project_id);
+    }
+	public function delete($id)
+	{
+		DB::table('events_attended')->where('member_id','=', $id)->delete();
+		//DB::insert('DELETE FROM `project` WHERE `id`=?', [$id]);
+		return back();
+		
+	}
+	public function search($project_id,$name)
+	{	
+		$hint="";
+		
+		$result = DB::select("SELECT `id`,`firstname`,`lastname`,`middlename`  
+		FROM `members`  WHERE  
+		`firstname` LIKE '%$name%' OR `lastname` LIKE '%$name%' OR `middlename` LIKE '%$name% '");
+		
+		foreach($result as $row){
+				$hint .= "<a href='addattendance/".$project_id."/".$row->id."'>".$row->firstname." ".$row->lastname."</a></br>";
+				//$hint++;
+		}
+		
+		if ($hint=="") {
+		  $response="no suggestion";
+		} else {
+		  $response= "<div style='border:1px solid;'>".$hint."</div>";
+		}
+		
+		echo $response;
+	}
+	public function updateEvent(Request $request)
+	{	
+	}
+	
+}
---
+++ resources/views/admin/event/index.blade.php
@@ -0,0 +1,57 @@
+@extends('backend.layouts.master')
+
+@section('page-header')
+    <h1>
+       Events
+        <small></small>
+    </h1>
+@endsection
+@section('content')
+	<div class="col-md-12">
+		<div class="box">
+        <div class="box-body">
+             <table class="table" id="dataTables-events">
+					<thead>
+						<tr>
+							 <th>ID</th>
+							 <th>Title</th>
+							 <th>Date Created</th>
+							 <th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach($data as $row){
+								$id = $row->id;
+								echo "<tr>
+											<td>".$id."</td>
+											<td><a href='attendance/".$id."'>".$row->name."</a></td>
+											<td>".$row->year."</td>
+											<td>
+												<a  class='btn btn-xs btn-primary'  href='event/EditProject/".$id."'><i class='fa fa-pencil' title='' data-placement='top' data-toggle='tooltip' data-original-title='Update'></i></a>
+												<a  class='btn btn-xs btn-danger'  href='event/deleteEvent/".$id."'><i class='fa fa-trash' title='' data-placement='top' data-toggle='tooltip' data-original-title='Delete'></i></a>
+											</td>
+										</tr>";
+							}
+						?>
+					</tbody>
+				</table>
+        </div><!-- /.box-body -->
+		</div><!--box box-success-->
+	</div>
+	
+@endsection
+@section('after-scripts-end')
+
+	<link href="{{ asset('tables/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css') }}" rel="stylesheet">
+	<link href="{{ asset('tables/datatables-responsive/css/dataTables.responsive.css') }}" rel="stylesheet">
+	<script src="{{ asset('tables/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ asset('tables/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js') }}"></script>
+	<script>
+		 $(document).ready(function() {
+			  $('#dataTables-events').DataTable({
+						 responsive: true
+			  });
+		 });
+	</script>
+@endsection
