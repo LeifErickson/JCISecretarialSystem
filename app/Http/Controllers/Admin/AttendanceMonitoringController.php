3422667267f98fa5ad07317ee8e7e3b2488aa34f
boan8 <albert_sandig@yahoo.com>
2016-05-25 15:57:04 +0800

    Automatic Generate

--- app/Http/Controllers/Admin/AttendanceMonitoringController.php
+++ app/Http/Controllers/Admin/AttendanceMonitoringController.php
@@ -15,7 +15,7 @@ class AttendanceMonitoringController extends Controller
 {
 	public function index($id)
 	{	
-		if($id == 0){
+		if($id == "0"){
 			$events = DB::select('SELECT `id`,`name` FROM `events`');
 			
 			$event_id = 0;
@@ -52,7 +52,6 @@ class AttendanceMonitoringController extends Controller
 			switch($choice[0]){
 				case "e":
 					$default = DB::select('SELECT `id`,`name` FROM `events` WHERE `id`=?',[$choice[1]]);
-					
 					$attendance = DB::select("SELECT t1.`id`,t1.`firstname`, t1.`lastname`,IFNULL(t2.Present,0) as Present 	FROM
 					(SELECT `members`.`id`,`firstname`, `lastname` FROM `members`) t1
 					LEFT JOIN 
@@ -65,7 +64,7 @@ class AttendanceMonitoringController extends Controller
 					) t2
 					ON
 					t1.`id` = t2.`id`",[$choice[1]]);
-					return view('admin.attendanceMonitoring.index',['events' => $events, 'attendance'=> $attendance,'title'=>$default]);
+					return view('admin.attendanceMonitoring.index',['projects' => $projects,'meetings' => $meetings,'events' => $events, 'attendance'=> $attendance,'title'=>$default]);
 					break;
 				case "p":
 					$default = DB::select('SELECT `id`,`name` FROM `events` WHERE `id`=?',[$choice[1]]);
@@ -77,12 +76,12 @@ class AttendanceMonitoringController extends Controller
 					FROM `projects_attended`,`members`
 					WHERE 
 					`projects_attended`.`member_id` = `members`.`id` AND
-					`projects_attended`.`event_id` = ?
+					`projects_attended`.`project_id` = ?
 					GROUP BY `members`.`id`
 					) t2
 					ON
 					t1.`id` = t2.`id`",[$choice[1]]);
-					return view('admin.attendanceMonitoring.index',['events' => $events, 'attendance'=> $attendance,'title'=>$default]);
+					return view('admin.attendanceMonitoring.index',['projects' => $projects,'meetings' => $meetings,'events' => $events, 'attendance'=> $attendance,'title'=>$default]);
 					break;
 				case "m":
 					
@@ -95,12 +94,12 @@ class AttendanceMonitoringController extends Controller
 					FROM `events_attended`,`members`
 					WHERE 
 					`events_attended`.`member_id` = `members`.`id` AND
-					`events_attended`.`event_id` = ?
+					`events_attended`.`meeting_id` = ?
 					GROUP BY `members`.`id`
 					) t2
 					ON
 					t1.`id` = t2.`id`",[$id]);
-					return view('admin.attendanceMonitoring.index',['events' => $events, 'attendance'=> $attendance,'title'=>$default]);
+					return view('admin.attendanceMonitoring.index',['projects' => $projects,'meetings' => $meetings,'events' => $events, 'attendance'=> $attendance,'title'=>$default]);
 					break;
 			}
 		}
