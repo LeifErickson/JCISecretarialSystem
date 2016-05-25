8e79e25f75525f71dad55ba5080e1c5ea6b01c17
boan8 <albert_sandig@yahoo.com>
2016-05-25 16:58:46 +0800

    Automatic Generate

--- app/Http/Controllers/Admin/MembersController.php
+++ app/Http/Controllers/Admin/MembersController.php
@@ -26,37 +26,103 @@ class MembersController extends Controller
 			
 			DB::insert('UPDATE  `members`
 						INNER JOIN  
-						(SELECT t1.`id`,IFNULL(t2.participate,0) as participate,((SELECT count(`id`) FROM `meetings` )+(SELECT count(`id`) FROM `events` )+(SELECT count(`id`) FROM `projects` )) as total 
-						FROM 
-						(SELECT `members`.`id` FROM `members` ) t1
-						LEFT JOIN
-						(SELECT `members`.`id`,count(`member_id`) as participate
-						FROM `events_attended`, `members`
-						WHERE
-							`events_attended`.`member_id` = `members`.`id`
-						GROUP BY 
-						`members`.`id`) t2
-						ON t1.`id` = t2.`id`) tableChange
+						(
+							SELECT events_proj.id,((events_proj.eve)+count(events_proj.proj)+count(meetings.participate)) AS Participation,((SELECT count(`id`) FROM `meetings` )+(SELECT count(`id`) FROM `events` )+(SELECT count(`id`) FROM `projects` )) as total 
+							FROM
+							(SELECT events.id as id, events.participate as eve,projects.participate as proj
+							FROM
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `events_attended`, `members`
+							WHERE
+								`events_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) events
+							LEFT JOIN
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `events_attended`, `members`
+							WHERE
+								`events_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) projects
+							ON projects.id = events.id) events_proj
+							LEFT JOIN
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `meetings_attended`, `members`
+							WHERE
+								`meetings_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) meetings
+							ON meetings.id = events_proj.id
+							GROUP BY events_proj.id
+) tableChange
 						ON `members`.`id` = tableChange.`id`
-						SET `members`.`memberstatus`=?
-						WHERE tableChange.participate/tableChange.total < 0.50',['inactive']);
+						SET `members`.`memberstatus`= ?
+						WHERE tableChange.Participation/tableChange.total < 0.50',['inactive']);
 						
 			DB::insert('UPDATE  `members`
 						INNER JOIN  
-						(SELECT t1.`id`,IFNULL(t2.participate,0) as participate,((SELECT count(`id`) FROM `meetings` )+(SELECT count(`id`) FROM `events` )+(SELECT count(`id`) FROM `projects` )) as total 
-						FROM 
-						(SELECT `members`.`id` FROM `members` ) t1
-						LEFT JOIN
-						(SELECT `members`.`id`,count(`member_id`) as participate
-						FROM `events_attended`, `members`
-						WHERE
-							`events_attended`.`member_id` = `members`.`id`
-						GROUP BY 
-						`members`.`id`) t2
-						ON t1.`id` = t2.`id`) tableChange
+						(
+						SELECT events_proj.id,((events_proj.eve)+count(events_proj.proj)+count(meetings.participate)) AS Participation,((SELECT count(`id`) FROM `meetings` )+(SELECT count(`id`) FROM `events` )+(SELECT count(`id`) FROM `projects` )) as total 
+							FROM
+							(SELECT events.id as id, events.participate as eve,projects.participate as proj
+							FROM
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `events_attended`, `members`
+							WHERE
+								`events_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) events
+							LEFT JOIN
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `events_attended`, `members`
+							WHERE
+								`events_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) projects
+							ON projects.id = events.id) events_proj
+							LEFT JOIN
+							(SELECT t1.`id` as id,IFNULL(t2.participate,0) as participate
+							FROM 
+							(SELECT `members`.`id` FROM `members` ) t1
+							LEFT JOIN
+							(SELECT `members`.`id`,count(`member_id`) as participate
+							FROM `meetings_attended`, `members`
+							WHERE
+								`meetings_attended`.`member_id` = `members`.`id`
+							GROUP BY 
+							`members`.`id`) t2
+							ON t1.`id` = t2.`id`) meetings
+							ON meetings.id = events_proj.id
+							GROUP BY events_proj.id
+						) tableChange
 						ON `members`.`id` = tableChange.`id`
 						SET `members`.`memberstatus`=?
-						WHERE tableChange.participate/tableChange.total > 0.50',['active']);
+						WHERE tableChange.Participation/tableChange.total > 0.50',['active']);
 
         return view('admin.members.index', compact('members'));
     }
